b50ff6d7268ab4637513044b1fb32b4be8f92fa4
Nabeel Farooq <45237521+Nabeel-Farooq@users.noreply.github.com>
2026-05-11 15:37:13 +0500

    Update middleware.py

--- app/middleware.py
+++ app/middleware.py
@@ -6,15 +6,33 @@ import app.modules.roxywi.auth as roxywi_auth
 import app.modules.roxywi.common as roxywi_common
 
 
+ALLOWED_SERVICES = (
+    'haproxy',
+    'nginx',
+    'apache',
+    'keepalived',
+    'cluster',
+    'waf',
+    'udp',
+)
+
+
 def check_services(fn):
     @wraps(fn)
     def decorated_view(*args, **kwargs):
-        service = kwargs['service']
-        if service not in ('haproxy', 'nginx', 'apache', 'keepalived', 'cluster', 'waf', 'udp'):
+        service = kwargs.get('service')
+
+        if service not in ALLOWED_SERVICES:
             abort(405, 'Wrong service')
+
         if not roxywi_auth.is_access_permit_to_service(service):
-            abort(403, f'You do not have needed permissions to access to {service.title()} service')
+            abort(
+                403,
+                f'You do not have needed permissions to access to {service.title()} service'
+            )
+
         return fn(*args, **kwargs)
+
     return decorated_view
 
 
@@ -23,13 +41,22 @@ def get_user_params(virt=0, disable=0):
         @wraps(fn)
         def decorated_views(*args, **kwargs):
             try:
-                user_params = roxywi_common.get_users_params(virt=virt, disable=disable, service=kwargs.get('service'))
+                user_params = roxywi_common.get_users_params(
+                    virt=virt,
+                    disable=disable,
+                    service=kwargs.get('service')
+                )
+
                 g.user_params = user_params
+
             except Exception as e:
                 print(e)
-                return abort(401)
+                abort(401)
+
             return fn(*args, **kwargs)
+
         return decorated_views
+
     return inner_decorator
 
 
@@ -38,10 +65,12 @@ def page_for_admin(level=1):
         @wraps(fn)
         def decorated_views(*args, **kwargs):
             if not roxywi_auth.is_admin(level=level):
-                return abort(403, 'bad permission')
-            else:
-                return fn(*args, **kwargs)
+                abort(403, 'bad permission')
+
+            return fn(*args, **kwargs)
+
         return decorated_views
+
     return inner_decorator
 
 
@@ -50,7 +79,13 @@ def check_group():
         @wraps(fn)
         def decorated_views(*args, **kwargs):
             if not roxywi_common.check_user_group_for_flask():
-                return roxywi_common.handle_json_exceptions('Wrong group', 'Cannot create user')
+                return roxywi_common.handle_json_exceptions(
+                    'Wrong group',
+                    'Cannot create user'
+                )
+
             return fn(*args, **kwargs)
+
         return decorated_views
+
     return inner_decorator
